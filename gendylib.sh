99acd28c8632cdbd5cc70d7ae8074b5f8c671031
刘家男 <liujianan01@58.com>
2019-09-06 11:12:44 +0800

    修改生成脚本

--- gendylib.sh
+++ gendylib.sh
@@ -1,3 +1,15 @@
-rm -rf swigfaiss4j.dylib
-/usr/local/opt/llvm/bin/clang++ -std=c++11 -fPIC -fopenmp -m64 -Wall -g -O3 -Wextra -msse4 -mpopcnt  -L/usr/local/lib -I../faiss/ -I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/ -lfaiss  swigfaiss4j.cpp -shared -o libswigfaiss4j.dylib
+# mac
+SHAREDEXT   = dylib
+# linux
+#SHAREDEXT   = dylib
+
+rm -rf swigfaiss4j.$SHAREDEXT
+
+# -L/usr/local/lib: libfaiss.so/dylib 所在目录
+# -I$JAVA_HOME/include/: 找jni.h
+# $JAVA_HOME/include/linux/: 找jni_md.h
+# linux
+# g++ -std=c++11 -fPIC -fopenmp -m64 -Wall -g -O3 -Wextra -msse4 -mpopcnt -fpermissive -L/usr/local/lib -I../faiss/ -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ -lfaiss  swigfaiss4j.cpp -shared -o libswigfaiss4j.$SHAREDEXT
+# mac
+/usr/local/opt/llvm/bin/clang++ -std=c++11 -fPIC -fopenmp -m64 -Wall -g -O3 -Wextra -msse4 -mpopcnt  -L/usr/local/lib -I../faiss/ -I$JAVA_HOME/include/ -I$JAVA_HOME/include/darwin/ -lfaiss  swigfaiss4j.cpp -shared -o libswigfaiss4j.$SHAREDEXT
 
--- swigc++2java.sh
+++ swigc++2java.sh
@@ -1,5 +1,7 @@
 rm -rf src/main/java/com/bj58/spider/faiss/*.java swigfaiss4j.cpp
 mkdir -p src/main/java/com/bj58/spider/faiss/
+# swig -version == 4.0
+# -I../faiss: 确保是faiss源码路径
 swig -c++ -java -package com.bj58.spider.faiss -o swigfaiss4j.cpp  -outdir src/main/java/com/bj58/spider/faiss/ -Doverride= -I../faiss/ swigfaiss4j.i
 
 
